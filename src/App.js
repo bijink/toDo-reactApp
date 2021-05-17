c579d40895d183158cc8fa3f252018aad12a44f3
Bijin <kbijin528@gmail.com>
2021-05-17 13:53:30 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import './App.css'
-import Header from './Componets/Header'
+
+
 function App() {
-  const name = 'Bijin';
   return (
     <div>
-      <Header name={name} />
-      <h1 style={{ color: 'blue' }} >Hello Gays!!!</h1>
-      <h1 className='hello' >Hello Gays!!!</h1>
-      <div>Hello</div>
+      
     </div>
   );
 }
--- src/Componets/Header.js
+++
@@ -1,6 +0,0 @@
-function Header(props) {
-   return (
-      <h1>This is a Header {props.name} </h1>
-   )
-}
-export default Header
