2ff4f80a4e7947ffe96b7aaf392e625268e2d6bc
Bijin <kbijin528@gmail.com>
2021-05-17 12:56:01 +0530

    Model File Stucture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Header from './Componets/Header'
 function App() {
+  const name = 'Bijin';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={name} />
+      <h1 style={{ color: 'blue' }} >Hello Gays!!!</h1>
+      <h1 className='hello' >Hello Gays!!!</h1>
+      <div>Hello</div>
     </div>
   );
 }
---
+++ src/Componets/Header.js
@@ -0,0 +1,6 @@
+function Header(props) {
+   return (
+      <h1>This is a Header {props.name} </h1>
+   )
+}
+export default Header
