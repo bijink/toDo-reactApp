fc4fde1bef2ec8bb4b0967d1313886329a0c99e7
Bijin K <kbijin528@gmail.com>
2021-06-19 14:43:25 +0530

    css changed

--- src/App.jsx
+++ src/App.jsx
@@ -12,37 +12,96 @@ function App() {
    const currDate = new Date();
    const timeDateDay = currDate.getHours() + ':' + currDate.getMinutes() + ':' + currDate.getSeconds() + ' ' + currDate.getDate() + '.' + currDate.getMonth() + 1 + '.' + currDate.getFullYear() + ' ' + dayNamesShort[currDate.getDay()];
 
+
    return (
       <div className="app">
-         <div className="mainHeading">
-            <h1>ToDo List</h1>
+
+         <div className="headings">
+            <div className="mainHeading">
+               <h1>ToDo List</h1>
+            </div>
+            <div className="subHeading">
+               <h2>Whoop, it's {day} 🌝 ☕ </h2>
+            </div>
          </div>
-         <div className="subHeading">
-            <br />
-            <h2>Whoop, it's {day} 🌝 ☕ </h2>
-         </div>
-         <div className="input">
+
+         <div className="toDoInput">
             <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
             <i onClick={() => setToDos([...toDos, { text: toDo, toDoTime: timeDateDay }])} className="fas fa-plus"></i>
          </div>
-         {
-            toDos.map((obj) => {
-               return (
-                  <div className="todos">
-                     <div style={{ float: 'left' }} className="todo">
+
+         <div className="container deleted">
+            <h3>Trash</h3>
+            {
+               toDos.map((obj) => {
+                  return (
+                     <div className="todo">
                         <div className="left">
                            <input type="checkbox" name="" id="" />
-                           <p>{obj.text}</p>
+                        </div>
+                        <div className="top">
+                           <p></p>
+                        </div>
+                        <div className="bottom">
+                           <p className="dateTimeDay"></p>
                         </div>
                         <div className="right">
-                           <p style={{ float: 'left', marginRight: '0.3rem' }}>{obj.toDoTime}</p>
-                           <i style={{ float: 'left' }} className="fas fa-times"></i>
+                           <i className="fas fa-times"></i>
                         </div>
                      </div>
-                  </div>
-               );
-            })
-         }
+                  );
+               })
+            }
+         </div>
+
+         <div className="container progressing">
+            <h3>On Going</h3>
+            {
+               toDos.map((obj) => {
+                  return (
+                     <div className="todo">
+                        <div className="left">
+                           <input type="checkbox" name="" id="" />
+                        </div>
+                        <div className="top">
+                           <p>{obj.text}</p>
+                        </div>
+                        <div className="bottom">
+                           <p>{obj.toDoTime}</p>
+                        </div>
+                        <div className="right">
+                           <i className="fas fa-times"></i>
+                        </div>
+                     </div>
+                  );
+               })
+            }
+         </div>
+
+         <div className="container completed">
+            <h3>Done</h3>
+            {
+               toDos.map((obj) => {
+                  return (
+                     <div className="todo">
+                        <div className="left">
+                           <input type="checkbox" name="" id="" />
+                        </div>
+                        <div className="top">
+                           <p></p>
+                        </div>
+                        <div className="bottom">
+                           <p className="dateTimeDay"></p>
+                        </div>
+                        <div className="right">
+                           <i className="fas fa-times"></i>
+                        </div>
+                     </div>
+                  );
+               })
+            }
+         </div>
+
       </div>
    );
 
